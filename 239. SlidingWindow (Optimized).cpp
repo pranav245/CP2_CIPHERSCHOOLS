9a2d1c29e0d2137df763dde998590547a72272aa
Pranav Patel <p.patelpranav245@gmail.com>
2020-08-16 11:27:41 +0530

    Optimized Solution

---
+++ 239. SlidingWindow (Optimized).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> v;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            while(!dq.empty() && nums[i]>dq.back()) dq.pop_back();
+            
+            dq.push_back(nums[i]);
+            
+            if(i>=k-1)
+            {
+                v.push_back(dq.front());
+                if(nums[i-k+1]==dq.front()) dq.pop_front();
+            }
+        }
+        
+        return v;
+    }
+};
