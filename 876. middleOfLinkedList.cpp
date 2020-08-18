91f48cf0f58e4344c704b936ba64388c8ec096ba
Pranav Patel <p.patelpranav245@gmail.com>
2020-08-18 20:55:05 +0530

    Only Function

---
+++ 876. middleOfLinkedList.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        struct ListNode *slow = head;
+        struct ListNode *fast = head;
+        
+        if(head!=NULL)
+        {
+            while(fast!=NULL and fast->next!=NULL)
+            {
+                fast = fast->next->next;
+                slow = slow->next;
+            }
+        }
+        return slow;
+        
+    }
+};
