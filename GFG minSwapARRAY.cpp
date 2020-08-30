1ff9d0ccf201398b7ddffa8a604e939121aca165
Pranav Patel <p.patelpranav245@gmail.com>
2020-08-30 13:25:40 +0530

    Full code

---
+++ GFG minSwapARRAY.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int minSwap(vector<int>& v, int num)
+{
+    int swaps = 0;
+    for (int i = 0, j = v.size() - 1; i < j;)
+    {
+        if (v[i] >= num && v[j] <= num)
+        {
+            swap(v[i], v[j]);
+            swaps++;
+            i++;
+            j--;
+        }
+        if (v[i] < num)
+            i++;
+        if (v[j] > num)
+            j--;
+    }
+    return swaps;
+}
+
+int main()
+{
+    int k, n;
+    cin >> k >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    
+    cout << minSwap(v, k);
+
+    return 0;
+}
